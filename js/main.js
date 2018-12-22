982a7d671fcb954447a3a4782f2aeb48c19585c3
Mikhail Shardin <kriptonit1988@gmail.com>
2018-12-22 20:45:38 +0500

    Отобразил фотографии пользователей

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@
 
 (function () {
 
+  updatePictures(window.data.generate());
+
+  function updatePictures(pictures) {
+    var fragment = preparePictures(pictures);
+    window.container.fill(fragment);
+  }
+
+  function preparePictures(pictures) {
+    var fragment = document.createDocumentFragment();
+    pictures.forEach(function (picture) {
+      var newPictureElement = window.picture.create(picture);
+      if (newPictureElement) {
+        fragment.appendChild(newPictureElement);
+      }
+    });
+
+    return fragment;
+  }
 })();
