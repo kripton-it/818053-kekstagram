2d12509cfecacff6b8eca36402b3e721f48bf7ab
Mikhail Shardin <kriptonit1988@gmail.com>
2018-12-24 17:45:24 +0500

    Реализовал изменение масштаба фотографии

--- js/effects.js
+++ js/effects.js
@@ -2,28 +2,66 @@
 
 (function () {
 
-  var effectsList = document.querySelector('.effects__list');
-  var previewElement = document.querySelector('.img-upload__preview img');
+  var effectsElement = document.querySelector('.effects__list');
+  var previewContainerElement = document.querySelector('.img-upload__preview');
+  var previewElement = previewContainerElement.querySelector('.img-upload__preview img');
   var defaultEffect = 'none';
-  var slider = document.querySelector('.img-upload__effect-level');
+  var sliderElement = document.querySelector('.img-upload__effect-level');
+  var scaleControlElement = document.querySelector('.img-upload__scale');
+  var scaleControlSmallerElement = scaleControlElement.querySelector('.scale__control--smaller');
+  var scaleControlBiggerElement = scaleControlElement.querySelector('.scale__control--bigger');
+  var scaleControlValueElement = scaleControlElement.querySelector('.scale__control--value');
+  var defaultScale = '100%';
 
   applyEffect(defaultEffect);
+  scaleControlValueElement.value = defaultScale;
+  updateScale();
 
-  effectsList.addEventListener('change', function () {
-    var activeEffect = effectsList.querySelector('input:checked').value;
+  scaleControlSmallerElement.addEventListener('click', function () {
+    updateScale('small');
+  });
+
+  scaleControlBiggerElement.addEventListener('click', function () {
+    updateScale('big');
+  });
+
+  effectsElement.addEventListener('change', function () {
+    var activeEffect = effectsElement.querySelector('input:checked').value;
     applyEffect(activeEffect);
   });
 
+  function updatePreviewScale() {
+    var scale = parseInt(scaleControlValueElement.value, 10) / 100;
+    previewContainerElement.style.transform = 'scale(' + scale + ')';
+  }
+
+  function updateScale(direction) {
+
+    switch (direction) {
+      case 'small':
+        scaleControlValueElement.value = (parseInt(scaleControlValueElement.value, 10) - 25) + '%';
+        break;
+      case 'big':
+        scaleControlValueElement.value = (parseInt(scaleControlValueElement.value, 10) + 25) + '%';
+        break;
+    }
+
+    var scaleValue = scaleControlValueElement.value;
+    scaleControlSmallerElement.disabled = scaleValue === '25%';
+    scaleControlBiggerElement.disabled = scaleValue === '100%';
+    updatePreviewScale();
+  }
+
   function applyEffect(effect) {
-    effectsList.querySelector('#effect-' + effect).checked = true;
+    effectsElement.querySelector('#effect-' + effect).checked = true;
     previewElement.className = '';
     previewElement.classList.add('effects__preview--' + effect);
     switch (effect) {
       case 'none':
-        slider.classList.add('hidden');
+        sliderElement.classList.add('hidden');
         break;
       default:
-        slider.classList.remove('hidden');
+        sliderElement.classList.remove('hidden');
         break;
     }
   }
