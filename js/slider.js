520b6f5558513e6e4dd37a367f90faed293b6498
Mikhail Shardin <kriptonit1988@gmail.com>
2018-12-28 00:21:24 +0500

    Реализовал перетаскивание слайдера

--- js/slider.js
+++ js/slider.js
@@ -7,25 +7,69 @@
   var sliderPinElement = slider.querySelector('.effect-level__pin');
   var sliderBarElement = slider.querySelector('.effect-level__depth');
   var sliderValueElement = slider.querySelector('.effect-level__value');
+  var sliderWidth;
+  var startPinPosition;
 
   updateSlider(sliderStartValue);
-  sliderPinElement.addEventListener('mouseup', onSliderPinMouseup);
+  sliderPinElement.addEventListener('mousedown', onSliderPinMousedown);
 
   function updateSlider(level) {
-    sliderPinElement.style.left = 100 * level + '%';
-    sliderBarElement.style.width = 100 * level + '%';
-    sliderValueElement.value = 100 * level + '%';
+    var zeroValue = '0';
+    var defaultValue = 100 * level + '%';
+    sliderPinElement.style.left = level > 0 ? defaultValue : zeroValue;
+    sliderBarElement.style.width = level > 0 ? defaultValue : zeroValue;
+    sliderValueElement.value = level > 0 ? defaultValue : zeroValue;
   }
 
-  function onSliderPinMouseup() {
-    updateSlider(getEffectDepth());
+  function onSliderPinMousedown(evt) {
+    evt.preventDefault();
+    sliderWidth = sliderPinElement.parentNode.offsetWidth;
+    startPinPosition = evt.clientX;
+
+    document.addEventListener('mousemove', onSliderPinMousemove);
+    document.addEventListener('mouseup', onSliderPinMouseup);
   }
 
-  function getEffectDepth() {
+  function onSliderPinMousemove(evt) {
+    evt.preventDefault();
+
+    var shift = evt.clientX - startPinPosition;
+
+    startPinPosition = evt.clientX;
+
+    if (getSliderLevel() < 0) {
+      updateSlider(0);
+    } else if (getSliderLevel() > 1) {
+      updateSlider(1);
+    } else {
+      console.log(sliderPinElement.offsetLeft);
+      console.log(shift);
+      console.log(sliderWidth);
+      console.log('********');
+      updateSlider((sliderPinElement.offsetLeft + shift) / (sliderWidth - 1));
+    }
+    /*
+        if (sliderPinMouseMoveCallback) {
+          sliderPinMouseMoveCallback();
+        }*/
+  }
+
+  function onSliderPinMouseup(evt) {
+    evt.preventDefault();
+
+    document.removeEventListener('mousemove', onSliderPinMousemove);
+    document.removeEventListener('mouseup', onSliderPinMouseup);
+    /*
+        if (sliderPinMouseUpCallback) {
+          sliderPinMouseUpCallback();
+        }*/
+  }
+
+  function getSliderLevel() {
     return parseInt(sliderPinElement.style.left, 10) / 100;
   }
 
   window.slider = {
-
+    level: getSliderLevel
   };
 })();
